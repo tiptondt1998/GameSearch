0807b430d963c5749b76c2bfa3ee3de5300a1b10
Kevin Eslick <eslick.kevin@yahoo.com>
2020-11-12 20:41:27 -0600

    added error handling

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # GameSearch
+
 Uses rawg api and nes css framework to get game data
+
+- [x] Framework other than Bootstrap
+- [x] Deployed to Github Pages.
+- [x] Be interactive
+- [x] Two API calls
+- [x] Do not use alerts, confirms or prompts.
+- [ ] Be responsive css
+- [ ] Have a polished UI
+- [ ] Clean repository
+
+# todos
+
+- check for date entered
+- error handlers - display if date entered is incorrect
+- css changes
+- localStorage implemented
+- clean up folder directories
--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 var displayHolder = document.getElementById('gameInfo')
-var gameDet = document.getElementById('gameName')
-var gameScore = document.getElementById('gameRating')
-var gameRel = document.getElementById('gameRelease')
-var gameDesc = document.getElementById('gameDesc')
+var games = document.getElementById("games");
+var display = document.getElementById('gameDisplay')
 
 var apiKey = "3f7a082ecdb74b2388222a89923e48d5";
 var getMostPopularGame = function(){
@@ -13,8 +11,23 @@ var getMostPopularGame = function(){
     var todayDate = year + "-" + month + "-" + day;
 
     var selectedYear = document.getElementById("inline_field").value;
-
-    var apiUrl = " https://api.rawg.io/api/games?dates="+selectedYear+"-01-01,"+selectedYear+"-12-31&ordering=-added";
+    var regex = /\d{4}/g
+    var found = selectedYear.match(regex)
+    
+    if(!found || selectedYear > year || selectedYear < 1960){
+      console.log('bad year')
+      games.innerHTML = ''
+      display.innerHTML = ''
+      var badDate = document.createElement('h1')
+      badDate.classList.add('nes-text')
+      badDate.classList.add('is-error')
+      badDate.innerHTML = 'bad date'
+      games.appendChild(badDate)
+    }else {
+    if (displayHolder.innerHTML != '') {
+      displayHolder.innerHTML = ''
+    }
+    var apiUrl = "https://api.rawg.io/api/games?dates="+selectedYear+"-01-01,"+selectedYear+"-12-31&ordering=-added";
     //make a request to the url
     fetch(apiUrl)
     .then(function(response) {
@@ -27,16 +40,15 @@ var getMostPopularGame = function(){
         alert("Error: " + response.statusText);
       }
     });
+    }
 }
 
 var displayMostPopularGames = function(data){
     console.log(data);
-    var games = document.getElementById("games");
+    
+    console.log('most popular games')
     games.innerHTML = ''
-    gameDet.innerHTML = ''
-    gameScore.innerHTML = ''
-    gameRel.innerHTML = ''
-    gameDesc.innerHTML = ''
+    display.innerHTML = ''
     var gamesArray = [];
     for(let i = 0; i<data.results.length; i++){
     gamesArray.push(data.results[i]);
@@ -54,7 +66,6 @@ var displayMostPopularGames = function(data){
       games.appendChild(gameHolder)	
       gameHolder.addEventListener('click',function (params) {
         fetchInfo(this.id)
-        gameDesc.scrollIntoView()
       })
 
     }
@@ -64,6 +75,7 @@ var displayMostPopularGames = function(data){
    
 }
 function fetchInfo(id) {
+  console.log('fetching game')
   fetch('https://api.rawg.io/api/games/' + id)
   .then(function(nameReturned) {
     // request was successful
@@ -78,11 +90,39 @@ function fetchInfo(id) {
 }
 
 function displayInfo(game) {
-  
+  console.log('displaying')
+  if (displayHolder.innerHTML != '') {
+    displayHolder.innerHTML = ''
+  }
+  var gameDetails = document.createElement('div')
+  gameDetails.classList.add('nes-text')
+  gameDetails.classList.add('is-primary')
+  gameDetails.innerHTML = 'Game Name = '+ game.name
 
-  gameDet.innerHTML = 'Game Name = '+ game.name
+  displayHolder.appendChild(gameDetails)
+
+  var gameScore = document.createElement('div')
+  gameScore.classList.add('nes-text')
+  gameScore.classList.add('is-primary')
   gameScore.innerHTML = 'Rating = ' + game.rating
-  gameRel.innerHTML = 'Release Date = ' + game.released
+
+  displayHolder.appendChild(gameScore)
+
+  var releaseDate = document.createElement('div')
+  releaseDate.classList.add('nes-text')
+  releaseDate.classList.add('is-primary')
+  releaseDate.innerHTML = 'Release Date = ' + game.released
+
+  displayHolder.appendChild(releaseDate)
+
+  var gameDesc = document.createElement('div')
+  gameDesc.classList.add('nes-text')
+  gameDesc.classList.add('is-primary')
   gameDesc.innerHTML = game.description
+
+  displayHolder.appendChild(gameDesc)
+
+  display.appendChild(displayHolder)
+
 }
 document.getElementById("mostAnticipated").addEventListener("click",getMostPopularGame);
